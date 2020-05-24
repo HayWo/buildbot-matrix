cc417d70e8fba7ea0a5600327b3f865e884a0411
haywo <haywo@nct08.de>
2020-05-24 15:45:19 +0200

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 with open("README.md", "r") as rm:
     long_description = rm.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 setup(name='buildbot-matrix',
         version=VERSION,
